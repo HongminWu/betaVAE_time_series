d8348df7c08ea8ed7fb671cf24b735bdddd96ffe
HongminWu <hongminwu0120@gmail.com>
2020-09-18 15:11:33 +0800

    add author info

--- betaVAE.py
+++ betaVAE.py
@@ -1,10 +1,11 @@
-# %%
+'''
+# author: Hongmin Wu
+# date: 2020-09-18
+'''
 import numpy as np
 import matplotlib.pylab as plt
 import random
 import pandas as pd
-import ipdb
-# %%
 import torch
 import torch.utils.data
 from torch import nn, optim
@@ -12,55 +13,39 @@ from torch.nn import functional as F
 from torch.autograd import Variable
 from torch.distributions import normal
 import torch.utils.data as utils
-
-# %%
+import ipdb
 from torch.optim import lr_scheduler
 
 # %%
 """
 # Loading data
 """
-
-# %%
-# Xc = np.load('./data/control_beats.npy')
-# Xp = np.load('./data/positive_beats.npy')
-
-# X = np.concatenate((Xc, Xp))
-
-X = np.load('./data/nsamples.npy')
-
+X_raw = np.load('./data/nsamples.npy')
 figure_path = './log/figures'
+Y = np.zeros(len(X_raw)) # label
+print(X_raw.shape)
 
-Y = np.zeros(len(X))
-
-# %%
-print(X.shape)
-
-# %%
 def remap(x, out_min, out_max):
     in_min, in_max = np.min(x), np.max(x)
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+X = np.array([remap(x, -1, 1) for x in X_raw])
 
 # %%
-X = np.array([remap(x, -1, 1) for x in X])
+NUM = 50
+fig, axarr = plt.subplots(nrows=2, ncols=1)
+for n in range(NUM):
+    axarr[0].plot(X_raw[n])
+    axarr[1].plot(X[n])
+axarr[0].set_title('original 20 samples')
+axarr[1].set_title('20 samples after remaping')
+fig.savefig(figure_path + '/samples.png',format='png')
 
-# %%
-plt.figure()
-for x in X[:20]:
-    plt.plot(x)
-plt.title('original 20 samples')
-plt.savefig(figure_path + '/samples.png',format='png')
 
-# %%
-
-train = torch.utils.data.TensorDataset(torch.Tensor(X), torch.Tensor(Y))
-train_loader = torch.utils.data.DataLoader(train, batch_size=64, shuffle=True) 
-
-# %%
 """
 # Neural network
 """
-
+train = torch.utils.data.TensorDataset(torch.Tensor(X), torch.Tensor(Y))
+train_loader = torch.utils.data.DataLoader(train, batch_size=64, shuffle=True) 
 # %%
 nn_dim = 50
 nn_dim2 = 25
@@ -72,12 +57,15 @@ class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
 
+        # encoder: mu
         self.fc1 = nn.Linear(img_shape, nn_dim)
+        # encder: sigma
         self.fc2 = nn.Linear(nn_dim, nn_dim2)
         
         self.fc21 = nn.Linear(nn_dim2, latent_dim)
         self.fc22 = nn.Linear(nn_dim2, latent_dim)
-        
+
+        # decoder
         self.fc3 = nn.Linear(latent_dim, nn_dim2)
         self.fc4 = nn.Linear(nn_dim2, nn_dim)
         self.fc5 = nn.Linear(nn_dim, img_shape)
@@ -100,7 +88,6 @@ class VAE(nn.Module):
         h3 = F.leaky_relu(self.fc3(z))
         h4 = F.leaky_relu(self.fc4(h3))
         return self.fc5(h4)
-#         return torch.tanh(self.fc5(h4))
 
     def forward(self, x):
         mu, logvar = self.encode(x.view(-1, img_shape))
@@ -110,16 +97,12 @@ class VAE(nn.Module):
 # %%
 def visualize(model, latent_dim, rangee, epoch):
     viz = []
-    
     random_index = np.random.randint(len(X)-1)
     X_random = X[random_index]
     zi, zi_std = model.encode(torch.Tensor(X_random))
-    
-#     zi = torch.randn(latent_dim)
-    
-    for j in range(latent_dim):    
+    for j in range(latent_dim): # for each latent
         buf = []   
-        for i in rangee:
+        for i in rangee: # updating the z by adding the value in rangee
             x = zi.clone()
             x[j] += i
             zi2 = torch.zeros(latent_dim)
@@ -128,38 +111,37 @@ def visualize(model, latent_dim, rangee, epoch):
             buf.append(gen_imgs_np)
         viz.append(buf)
     
-    N, M = 5, int(latent_dim / 5) # latent_dim = M x N. 
-    fig, axs = plt.subplots(M, N, sharex=True, sharey=True, figsize=(int(N*4), int(M*4)))
     lw = 2
-    
-    k = 0
+    N, M = 5, int(latent_dim / 5) # latent_dim = M x N. 
+    cmap = plt.get_cmap('RdYlGn_r')
+    colors = cmap(np.linspace(0, 1.0,len(rangee)).tolist())
+    fig, axs = plt.subplots(M, N, sharex=True, sharey=True, figsize=(int(N*4), int(M*4)))
+
+    lantent_idx = 0
     for i in range(M): # M: rows
         for j in range(N): # N: columns
             axs[i, j].plot(X_random, ls = '--', color = 'black', label='sample')
-            ts = np.array(viz[k]).T
-            axs[i, j].plot(ts[:, 0], lw = lw, color = 'red',   label='z_%s=%s'%(k, rangee[0]))
-            axs[i, j].plot(ts[:, 1], lw = lw, color = 'green', label='z_%s=%s'%(k, rangee[1]))
-            axs[i, j].plot(ts[:, 2], lw = lw, color = 'blue',  label='z_%s=%s'%(k, rangee[2]))
-            axs[i, j].set_title('latent: z_%s'%k)
+            ts = np.array(viz[lantent_idx]).T
+            for k, value in enumerate(rangee):
+                axs[i, j].plot(ts[:,k], lw=lw, color=colors[k], label='z_%s+=%s'%(lantent_idx, rangee[k]))
+            axs[i, j].set_title('Updating z_%s, and others fixed'%lantent_idx)
             axs[i, j].legend(bbox_to_anchor=(0, 0.02), loc=3, borderaxespad=0.2, ncol=2)
-            k += 1
-    fig.suptitle('Training: epoch_%s with total %s latent states'%(epoch,latent_dim))
+            lantent_idx += 1
+    fig.suptitle('Training: epoch_%s with total %s latent states = %s'%(epoch,latent_dim, str(np.round(zi.tolist(),2))))
     fig.savefig(figure_path + '/epoch_%s.png'%epoch, format="png")
-    # plt.show()
     
-
-def visualize_beat_reconstruction(X, model):
+def visualize_reconstruction_from_trained_model(X, model):
     I = torch.randint(len(X)-1, (1, ))
     x_input = torch.Tensor(np.array([X[I]]))
     x_vector = model.encode(x_input)
     x_reconstruct = model.decode(model.reparameterize(x_vector[0], x_vector[1]))
 
     plt.figure()
-    plt.title('Random reconstructed beat')
+    plt.title('Random reconstructed sample')
     plt.plot(X[I], label='sample')
     plt.plot(x_reconstruct.data.numpy()[0], label = 'reconstructed')
     plt.legend()
-    plt.savefig(figure_path + '/random_reconstructed_beat.png')
+    plt.savefig(figure_path + '/random_reconstructed_sample.png')
 
 # %%
 """
@@ -175,9 +157,7 @@ def loss_function(recon_x, x, mu, logvar):
 
 # %%
 epochs = 100
-
 beta = 100
-
 C, C_final = 0, 25
 C_stop = epochs #25
 C_delta = (1 / C_stop) * C_final
@@ -197,9 +177,7 @@ for epoch in range(epochs):
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data
         optimizer.zero_grad()
-        
         recon_batch, mu, logvar = model(data)
-        
         try:
             BCE, KLD = loss_function(recon_batch, data, mu, logvar)
             BCE, KLD = BCE / len(data), KLD / len(data)
@@ -214,7 +192,6 @@ for epoch in range(epochs):
         if epoch > C_stop:
             C = C_final
 
-#     if epoch % 10 == 0:                
     print('Epoch', str(epoch), 
           '| BCE', BCE.data.numpy(), 
           '| KL', KLD.data.numpy(), 
@@ -232,13 +209,13 @@ for epoch in range(epochs):
 
     C += C_delta
 
+#------model trained-------------------------------------------------------------------------------------------------------------
+    
 # %%
 viz = []
-
 random_index = np.random.randint(len(X)-1)
 X_random = X[random_index]
 zi, zi_std = model.encode(torch.Tensor(X_random))
-# zi = torch.randn(latent_dim)
 rangee = np.arange(-3, 4, 1.5)
 for j in range(latent_dim):    
     buf = []   
@@ -251,35 +228,26 @@ for j in range(latent_dim):
         buf.append(gen_imgs_np)
     viz.append(buf)
 
-N, M = 5, int(latent_dim / 5)
-fig, axs = plt.subplots(M, N, sharex=True, sharey=True, figsize=(int(N*4), int(M*4)))
 lw = 2
+N, M = 5, int(latent_dim / 5) # latent_dim = M x N. 
+cmap = plt.get_cmap('RdYlGn_r')
+colors = cmap(np.linspace(0, 1.0,len(rangee)).tolist())
+fig, axs = plt.subplots(M, N, sharex=True, sharey=True, figsize=(int(N*4), int(M*4)))
 
-k = 0
-for i in range(M): # rows
-    for j in range(N): # columns
+lantent_idx = 0
+for i in range(M): # M: rows
+    for j in range(N): # N: columns
         axs[i, j].plot(X_random, ls = '--', color = 'black', label='random sample')
-        
-        ts = np.array(viz[k]).T
-        axs[i, j].plot(ts[:, 0], lw = lw, color = 'red',    label='z_%s=%s'%(k, rangee[0]))
-        axs[i, j].plot(ts[:, 1], lw = lw, color = 'green',  label='z_%s=%s'%(k, rangee[1]))
-        axs[i, j].plot(ts[:, 2], lw = lw, color = 'blue',   label='z_%s=%s'%(k, rangee[2]))
-        axs[i, j].plot(ts[:, 3], lw = lw, color = 'gold',   label='z_%s=%s'%(k, rangee[3]))
-        axs[i, j].plot(ts[:, 4], lw = lw, color = 'grey',   label='z_%s=%s'%(k, rangee[4]))
-        axs[i, j].set_title('latent: z_%s'%k)
-        axs[i, j].legend(bbox_to_anchor=(0, 0.02), loc=3, borderaxespad=0.2, ncol=2)        
-        k += 1
+        ts = np.array(viz[lantent_idx]).T
+        for k, value in enumerate(rangee):
+            axs[i, j].plot(ts[:,k], lw=lw, color=colors[k], label='z_%s+=%s'%(lantent_idx, rangee[k]))
+        axs[i, j].set_title('Updating z_%s, and others fixed'%lantent_idx)
+        axs[i, j].legend(bbox_to_anchor=(0, 0.02), loc=3, borderaxespad=0.2, ncol=2)
+        lantent_idx += 1
 fig.suptitle("Trained with total of %s latent states"%latent_dim)        
 fig.savefig(figure_path + '/result.png', format='png')
-# plt.show()
 
-visualize_beat_reconstruction(X, model)
-
-# plot the training process
-# list_of_KL  = []
-# list_of_C   = []
-# list_of_KLC_beta = []
-# list_of_loss = []
+visualize_reconstruction_from_trained_model(X, model)
 
 fig, ax = plt.subplots()
 ax.plot(list_of_BCE, label='BCE')
@@ -306,3 +274,4 @@ ax.plot(list_of_loss, label='loss')
 plt.legend()
 fig.savefig(figure_path + '/loss.png', format='png')
 
+print("results saved")
